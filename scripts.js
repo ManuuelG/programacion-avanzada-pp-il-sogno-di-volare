69ac3dd9aadf60ff04d2ff79f9cd281b7fdb846d
ManuuelG <manuelguerreron93@gmail.com>
2023-10-09 17:32:46 +0200

    solucion

--- scripts.js
+++ scripts.js
@@ -30,36 +30,70 @@ const militaryHelicopter = [
     'assets/helicoptero-militar-5.jpg'
 ];
 
+
 class Gallery {
     constructor(civilImages, militaryImages) {
+     
+       this.civilImages = civilImages
+       this.militaryImages = militaryImages
     }
     
     getRandomCivil() {
+
+        const randomIndex = Math.floor(Math.random() * this.civilImages.length);
+        return this.civilImages[randomIndex];
+
     }
     
     getRandomMilitary() {
+
+    const randomIndex = Math.floor(Math.random() * this.militaryImages.length);
+    return this.militaryImages[randomIndex];
+
     }
     
     getAll() {
+
+         return this.civilImages, this.militaryImages
+    
+
     }
 }
 
 class Painter {
-    constructor() {
+    constructor(createGallery) {
+        this.createGallery()
     }
 
     createGallery() {
+    const section = document.createElement ('section')
+    section.classList.add('Gallery')
+    document.body.appendChild(section)
+    this.section = section;
+    
     }
 
     createImageTag(imageUrl) {
+    const img = document.createElement ('img')
+    img.src = imageUrl
+    return img
+
     }
 
     paintSingleImage(imageUrl) {
+    const imgSingle = this.createImageTag(imageUrl)
+    this.section.appendChild(imgSingle)
     }
 
     paintMultipleImages(arrayOfImages) {
-    }
+        arrayOfImages.forEach((imageUrl) => {
+            const imgSingle = this.createImageTag(imageUrl)
+            this.section.appendChild(imgSingle) 
+        }
+    )}
 }
+
+
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
